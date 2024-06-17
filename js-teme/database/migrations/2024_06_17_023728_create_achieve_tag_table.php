553975a3bb5e78526e19a53b48ff753babe6de3f
kikuchihibiki <113155569+kikuchihibiki@users.noreply.github.com>
2024-06-17 11:56:57 +0900

    マイグレーションファイルの作成

---
+++ js-teme/database/migrations/2024_06_17_023728_create_achieve_tag_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('achieve_tag', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('achieve_name');
+            $table->string('img');
+            $table->string('game');
+            $table->boolean('delete_flag')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('achieve_tag');
+    }
+};
