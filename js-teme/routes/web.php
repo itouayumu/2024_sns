d00d1b6bf2d1a23496a009ae9e358624a4a25818
itou <a.itou.sys22@morijyobi.ac.jp>
2024-06-17 09:46:12 +0900

    auht構築完了

--- js-teme/routes/web.php
+++ js-teme/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\Controller;
+use App\Http\Controllers\testcontroller;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
