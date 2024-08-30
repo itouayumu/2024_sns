b759ab9393fc80ad5637f09b0a9f734b90d3439c
kikuchihibiki <113155569+kikuchihibiki@users.noreply.github.com>
2024-08-30 10:13:00 +0900

    フォーマット変更

--- js-teme/app/Models/Post.php
+++ js-teme/app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Carbon\Carbon;
 
 class Post extends Model
 {
@@ -12,7 +13,10 @@ class Post extends Model
     protected $table = 'post';
 
     protected $fillable = [
-        'user_id', 'content', 'img', 'delete_flag'
+        'user_id',
+        'content',
+        'img',
+        'delete_flag'
     ];
 
     /**
@@ -26,5 +30,9 @@ class Post extends Model
     public function userInfo()
     {
         return $this->belongsTo(UserInformation::class, 'user_id', 'user_id');
+    }
+    public function getCreatedAtAttribute($value)
+    {
+        return Carbon::parse($value)->format('Y-m-d H:i:s');
     }
 }
