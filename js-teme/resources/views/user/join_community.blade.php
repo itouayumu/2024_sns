8200d86b14244fbebe32d9c53fd751a5ffbf9b9c
itou <a.itou.sys22@morijyobi.ac.jp>
2024-09-02 10:15:16 +0900

    仮完成

--- js-teme/resources/views/user/join_community.blade.php
+++ js-teme/resources/views/user/join_community.blade.php
@@ -5,6 +5,7 @@
 @section('css')
 <link rel="stylesheet" href="{{ asset('/css/auth.css') }}">
 <link rel="stylesheet" href="{{ asset('/css/community.css') }}">
+<link rel="stylesheet" href="{{ asset('/css/join.css') }}">
 
 <link rel="dns-prefetch" href="//fonts.bunny.net">
 <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
@@ -12,17 +13,17 @@
 
 
 @section('content')
-<h1>{{ $community->community_name }}</h1>
-<img src="{{ asset('/storage/images/' .$community->icon) }}" alt="コミュニティ画像" class="community-image">
-{{ $community->user->name }}
-{{ $community->genre->name }}
-{{ $community->comu_explanation}}
-{{ $community->id }}
-@if(!$join_flag)
-<form action="{{ route('join_function', ['community_id' => $community->id]) }}" method="post">
-@csrf
-    <button type="submit">参加する</button>
-</form>
-@else
-参加中です<a href="/talk">参加中のコミュニティへ</a>
-@endif
+<div class="main">
+
+    <h1>{{ $community->community_name }}</h1>
+    <img src="{{ asset('/storage/images/' .$community->icon) }}" alt="コミュニティ画像" class="community-image">
+
+    @if(!$join_flag)
+    <form action="{{ route('join_function', ['community_id' => $community->id]) }}" method="post">
+        @csrf
+        <button type="submit">参加する</button>
+    </form>
+    @else
+    <p>参加中です<a href="/talk"><button>参加中のコミュニティへ</button></a></p>
+    @endif
+</div>
